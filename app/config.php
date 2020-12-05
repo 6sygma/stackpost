665e6fc385b905750bd543cdb9ec2a3b4cb39c90
6sygma <73327762+6sygma@users.noreply.github.com>
2020-12-05 23:38:27 +0530

    New Project

---
+++ app/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+define('DB_NAME', 'stackposts');
+define('TIMEZONE', 'Pacific/Niue');
+define('ENCRYPTION_KEY', 'd1c50ac963fa22144ab24ad1235383f8');
